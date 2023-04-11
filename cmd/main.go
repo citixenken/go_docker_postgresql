bc4449d51c77bec355161ce9459759f604be243e
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-04-11 05:12:43 +0300

    feat: Adding a Postgres service

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
-
+import (
+	"github.com/citixenken/go_docker_postgresql/database"
+	"github.com/gofiber/fiber/v2"
+)
 func main() {
+    database.ConnectDb()
+
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
