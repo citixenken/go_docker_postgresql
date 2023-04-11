bc5cdc6339b61ced77c5c3123c252adc07948361
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-04-11 04:28:07 +0300

    feat: live reload for Go apps using Air

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM golang:1.20.2
 
 WORKDIR /usr/src/app
 
+# handle hot reloading/live reload for Go apps
+RUN go install github.com/cosmtrek/air@latest
+
 # copy files from host into container
 COPY . .
 
--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,7 @@ func main() {
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Greetings, @citixenken!")
+        return c.SendString("Hello, @citixenken!")
     })
 
     app.Listen(":3000")
